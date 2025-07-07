082ceccb86f63693bb7f4026e4697f3679ecbbd9
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-07-07 19:02:54 -0300

    Inicio aula 4

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const DataBase = require("../database/DataBase");
+const TipoProdutoModel = require("../models/TipoProdutoModel");
+const ProdutoModel = require("../models/ProdutoModel");
 
 router.get("/api/produto", async (request, response) => {
-    const result = await DataBase.executeSQLQuery("SELECT * FROM Produto");
+    const result = await ProdutoModel.findAll();
     return response.send(result);
 });
 router.get("/api/tipoproduto", async (request, response) => {
-    const result = await DataBase.executeSQLQuery("SELECT * FROM TipoProduto");
+    const result = await TipoProdutoModel.findAll();
     return response.send(result);
 });
 
