768a011cf744e5523632f35c57845133bb89a9dc
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-07-14 20:12:08 -0300

    modificando o arquivo de controladores

--- controllers_web/WebTipoProdutoController.js
+++ controllers_web/WebTipoProdutoController.js
@@ -1,64 +1,68 @@
-const ProdutoModel = require("../models/ProdutoModel");
 const TipoProdutoModel = require("../models/TipoProdutoModel");
-class WebProdutoController {
+
+class WebTipoProdutoController {
     /**
      * Mostra uma tela com todos os recursos
      * @param {*} req Requisição da rota do express
      * @param {*} res Resposta da rota do express
      */
     async index(req, res) {
+        $tipoProdutos = await TipoProdutoModel.findAll();
+        return res.render("tipoproduto/index", { $tipoProdutos });
     }
 
     /**
-    * Mostra um formulário para criação de um novo recurso
-    * @param {*} req Requisição da rota do express
-    * @param {*} res Resposta da rota do express
-    */
+     * Mostra um formulário para criação de um novo recurso
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     */
     async create(req, res) {
     }
 
     /**
-    * Salva um novo recurso no banco de dados
-    * @param {*} req Requisição da rota do express
-    * @param {*} res Resposta da rota do express
-    */
+     * Salva um novo recurso no banco de dados
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     */
     async store(req, res) {
     }
 
     /**
-    * Mostra um recurso específico
-    * @param {*} req Requisição da rota do express
-    * @param {*} res Resposta da rota do express
-    * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
-    */
+     * Mostra um recurso específico
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
     async show(req, res) {
     }
 
     /**
-    * Mostra um formulário para editar um recurso específico
-    * @param {*} req Requisição da rota do express
-    * @param {*} res Resposta da rota do express
-    * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
-    */
+     * Mostra um formulário para editar um recurso específico
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
     async edit(req, res) {
     }
 
     /**
-    * Atualiza um recurso existente no banco de dados
-    * @param {*} req Requisição da rota do express
-    * @param {*} res Resposta da rota do express
-    * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
-    */
+     * Atualiza um recurso existente no banco de dados
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
     async update(req, res) {
     }
 
     /**
-    * Remove um recurso existente do banco de dados
-    * @param {*} req Requisição da rota do express
-    * @param {*} res Resposta da rota do express
-    * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
-    */
+     * Remove um recurso existente do banco de dados
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
     async destroy(req, res) {
     }
 }
-module.exports = new WebProdutoController();
+
+module.exports = new WebTipoProdutoController();
+
