02f47b77ab86f1207d1e8201f40a5ec3fd45adf5
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-07-14 20:01:11 -0300

    modificando o arquivo de controladores

---
+++ controllers_web/WebProdutoController.js
@@ -0,0 +1,65 @@
+const TipoProdutoModel = require("../models/TipoProdutoModel");
+
+class WebTipoProdutoController {
+    /**
+     * Mostra uma tela com todos os recursos
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     */
+    async index(req, res) {
+    }
+
+    /**
+     * Mostra um formulário para criação de um novo recurso
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     */
+    async create(req, res) {
+    }
+
+    /**
+     * Salva um novo recurso no banco de dados
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     */
+    async store(req, res) {
+    }
+
+    /**
+     * Mostra um recurso específico
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
+    async show(req, res) {
+    }
+
+    /**
+     * Mostra um formulário para editar um recurso específico
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
+    async edit(req, res) {
+    }
+
+    /**
+     * Atualiza um recurso existente no banco de dados
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
+    async update(req, res) {
+    }
+
+    /**
+     * Remove um recurso existente do banco de dados
+     * @param {*} req Requisição da rota do express
+     * @param {*} res Resposta da rota do express
+     * @param {Number} req.params.tipoProdutoId Parâmetro passado pela rota do express
+     */
+    async destroy(req, res) {
+    }
+}
+
+module.exports = new WebTipoProdutoController();
